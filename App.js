66e3415f7eea05daf759e6ade2b8476cc213cd77
Christian Ainsley Del Rosario <ainsley.drosario@gmail.com>
2023-11-20 00:21:55 +0800

    added message utlils and list

--- App.js
+++ App.js
@@ -1,19 +1,54 @@
 import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 import Skeleton from "./views/Skeleton";
 import Status from "./components/Status";
+import MessageList from "./components/MessageList";
+import {
+    createImageMessage,
+    createLocationMessage,
+    createTextMessage,
+} from "./utils/MessageUtils";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Status />
-      <Skeleton />
-    </View>
-  );
+export default class App extends React.Component {
+    state = {
+        messages: [
+            createImageMessage("https://unsplash.it/300/300"),
+            createTextMessage("World"),
+            createTextMessage("Hello"),
+            createLocationMessage({
+                latitude: 37.78825,
+                longitude: -122.4324,
+            }),
+        ],
+    };
+
+    handlePressMessage = () => {};
+
+    renderMessageList = () => {
+        const { messages } = this.state;
+        return (
+            <View style={styles.content}>
+                <MessageList
+                    messages={messages}
+                    onPressMessage={this.handlePressMessage}
+                />
+            </View>
+        );
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Status />
+                <Skeleton />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#ffffff",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#ffffff",
+    },
 });
---
+++ components/MessageList.js
@@ -0,0 +1,88 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {
+    FlatList,
+    StyleSheet,
+    Image,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native";
+import { MessageShape } from "../utils/MessageUtils";
+import { MapView } from "expo";
+
+const keyExtractor = (item) => item.id.toString();
+
+export default class MessageList extends React.Component {
+    static propTypes = {
+        messages: PropTypes.arrayOf(MessageShape).isRequired,
+        onPressMessage: PropTypes.func,
+    };
+
+    static defaultProps = {
+        onPressMessage: () => {},
+    };
+
+    //...
+
+    renderMessageItem = ({ item }) => {
+        const { onPressMessage } = this.props;
+        return (
+            <View key={item.id} style={styles.messageRow}>
+                <TouchableOpacity onPress={() => onPressMessage(item)}>
+                    {this.renderMessageBody(item)}
+                </TouchableOpacity>
+            </View>
+        );
+    };
+
+    renderMessageBody = ({ type, text, uri, coordinate }) => {
+        switch (type) {
+            case "text":
+                return (
+                    <View style={styles.messageBubble}>
+                        <Text style={styles.text}>{text}</Text>
+                    </View>
+                );
+
+            case "image":
+                return <Image style={styles.image} source={{ uri }} />;
+            case "location":
+                return (
+                    <MapView
+                        style={styles.map}
+                        initialRegion={{
+                            ...coordinate,
+                            latitudeDelta: 0.08,
+                            longitudeDelta: 0.04,
+                        }}
+                    >
+                        <MapView.Marker coordinate={coordinate} />
+                    </MapView>
+                );
+            default:
+                return null;
+        }
+    };
+
+    render() {
+        const { messages } = this.props;
+        return (
+            <FlatList
+                style={styles.container}
+                inverted
+                data={messages}
+                renderItem={this.renderMessageItem}
+                keyExtractor={keyExtractor}
+                keyboardShouldPersistTaps={"handled"}
+            />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        overflow: "visible", // prevents clipping on resize!
+    },
+});
